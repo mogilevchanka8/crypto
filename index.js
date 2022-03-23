47c7204e4807e24b1e758b92d43b27c9125b6762
mogilevchanka8 <98305927+mogilevchanka8@users.noreply.github.com>
2022-03-23 10:08:31 +0300

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,26 @@
+funtion Blok(letter, rate, used, father, code) {
+  this.letter = letter
+  this.rate = rate
+  this.used = used
+  this.father = father
+  this.code = code
+  }
+  
+  function FindCode(block) {
+  
+    if (tree[block.father].code  != ' ') {
+      block.code = tree[block.father].code + '1';
+    }
+      else {
+      if (block.letter == tree[minIndex].letter) {
+        block.code = '0';
+    }
+          else if (block.letter = tree[premionIndex].letter) {
+        block.code = '1'; 
+    }
+          else {
+        FindCode(tree[block.father]);
+        block.code = tree[block.father].code + '0';
+    }   
+  }
+} 
